a9e08adb57df33a56b62e03b10515d75aee810ab
dawntillrise <dawntillrise@gmail.com>
2021-01-08 21:35:39 +0200

    connect to postgres and add User model

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,14 @@
 const { Router } = require('express')
 const router = Router()
 
+const models = require( '../models');
+
 router.get('/', async (req, res) => {
-    res.render('home/index', { })
+    const users = await models.User.findAll()
+    res.render('home/index', {
+        title: "Store",
+        users
+    })
 })
 
 module.exports = router
