1b59dfc65d64eba3f0651e9badf2aadcd4ed003b
dawntillrise <dawntillrise@gmail.com>
2021-01-08 11:26:51 +0200

    add home page

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 var expressVue = require("express-vue");
 const express = require('express')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
 const path = require('path')
 
 require('dotenv').config()
 
 const PORT = process.env.PORT || 80;
 
+const app = express()
+
+const hbs = exphbs.create({
+	defaultLayout: 'main',
+	extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(homeRoutes)
+
+app.listen(PORT, () => {
+    console.log('Server has been started...')
+})
