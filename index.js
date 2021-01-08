1a4b8fd09ffaec26a83f7f3d42dd5ad73f9ada93
dawntillrise <dawntillrise@gmail.com>
2021-01-08 10:44:51 +0200

    add dotenv config

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var expressVue = require("express-vue");
 const express = require('express')
-const mongoose = require('mongoose')
 const path = require('path')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 80;
+
