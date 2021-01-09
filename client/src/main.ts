c550a2e6aabdf577e6baaab4bb24e1776517fd4b
dawntillrise <dawntillrise@gmail.com>
2021-01-09 08:40:38 +0200

    add sign_up sign_in forgot pages

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,9 @@
+
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/scss/main.scss'
 
 createApp(App).use(router).mount('#app')
